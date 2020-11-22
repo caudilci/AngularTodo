9068e8cb5bdb2ee8e34651972f46187994e430e8
Chris Caudill <christopheriancaudill@gmail.com>
2020-11-22 12:20:22 -0800

    added checking for completion and stubbed tog/del

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -16,4 +16,21 @@ export class TodoItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes;
+  }
+
+  onToggle(todo:Todo){
+    todo.completed = !todo.completed;
+  }
+
+  onDelete(todo:Todo){
+
+  }
+
 }
